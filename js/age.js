26d228a9a39c6d209ff033663a0ad4d8e8d23658
LukeHxH <lucasmed812@gmail.com>
2018-10-19 19:44:00 -0300

    lorem ipsum to test

---
+++ js/age.js
@@ -0,0 +1,16 @@
+function setAge() {
+	var today = new Date();
+	var birth = new Date("06/29/1998");
+	var age = today.getFullYear() - birth.getFullYear();
+	var m = today.getMonth() - birth.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
+        age--;
+    }
+
+    return age;
+}
+
+window.onload = function() {
+	var ageField = document.getElementById("myAge");
+	ageField.innerHTML = setAge();
+};
